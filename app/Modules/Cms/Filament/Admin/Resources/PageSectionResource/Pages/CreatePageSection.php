9717568a626f2da67c38a4ce71d36881622d1031
Solomon Gbeta <35314219+solomongs@users.noreply.github.com>
2026-07-12 10:00:19 +0100

    feat(cms): add page section creation screen

---
+++ app/Modules/Cms/Filament/Admin/Resources/PageSectionResource/Pages/CreatePageSection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Cms\Filament\Admin\Resources\PageSectionResource\Pages;
+
+use App\Modules\Cms\Filament\Admin\Resources\PageSectionResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePageSection extends CreateRecord
+{
+    protected static string $resource = PageSectionResource::class;
+}
