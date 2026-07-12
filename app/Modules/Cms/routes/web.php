22d20d15a2bd222254daf99fcda4c9a8d1eec8d3
Solomon Gbeta <35314219+solomongs@users.noreply.github.com>
2026-07-12 11:37:23 +0100

    feat(cms): register sitemap and legacy redirect routes

--- app/Modules/Cms/routes/web.php
+++ app/Modules/Cms/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Modules\Cms\Http\Controllers\PageController;
+use App\Modules\Cms\Http\Controllers\RedirectController;
+use App\Modules\Cms\Http\Controllers\SitemapController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/sitemap.xml', SitemapController::class)
+    ->name('cms.sitemap');
 
 Route::get('/cms-preview/{page:id}', [PageController::class, 'preview'])
     ->middleware(['auth:sanctum', config('jetstream.auth_session')])
@@ -18,3 +23,7 @@ $reservedPattern = $reservedSlugs === []
 Route::get('/{slug}', [PageController::class, 'show'])
     ->where('slug', '^'.$reservedPattern.'[a-z0-9][a-z0-9-]*$')
     ->name('cms.pages.show');
+
+Route::get('/{redirectPath}', RedirectController::class)
+    ->where('redirectPath', '.+')
+    ->name('cms.redirects.resolve');
