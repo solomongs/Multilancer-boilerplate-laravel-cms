2464659a9e74027195285d036a639276e6961b71
Solomon Gbeta <35314219+solomongs@users.noreply.github.com>
2026-07-12 11:38:01 +0100

    feat(cms): add redirect creation screen

---
+++ app/Modules/Cms/Filament/Admin/Resources/RedirectResource/Pages/CreateRedirect.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Cms\Filament\Admin\Resources\RedirectResource\Pages;
+
+use App\Modules\Cms\Filament\Admin\Resources\RedirectResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRedirect extends CreateRecord
+{
+    protected static string $resource = RedirectResource::class;
+}
