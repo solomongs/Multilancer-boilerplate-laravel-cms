2be92fed4be2172b854d28092c04ff9880914289
Solomon Gbeta <35314219+solomongs@users.noreply.github.com>
2026-07-12 09:45:12 +0100

    feat: add structured M2026 homepage template

---
+++ themes/m2026/views/pages/home.blade.php
@@ -0,0 +1,21 @@
+@extends(theme_layout('app'))
+
+@section('title', $page->meta_title ?? $page->title ?? config('app.name', 'Multilancer Limited'))
+@section('meta_description', $page->meta_description ?? '')
+
+@section('content')
+    @forelse (($sections ?? []) as $section)
+        @php
+            $type = is_array($section) ? ($section['type'] ?? null) : ($section->section_type ?? null);
+            $data = is_array($section) ? ($section['content'] ?? []) : ($section->content ?? []);
+        @endphp
+
+        @if (is_string($type) && preg_match('/^[a-z0-9_-]+$/', $type))
+            @includeIf("sections.{$type}", ['section' => $section, 'content' => $data])
+        @endif
+    @empty
+        <section data-m2026-placeholder="home">
+            {{-- The audited M2026 homepage sections will replace this scaffold. --}}
+        </section>
+    @endforelse
+@endsection
